4f84690591abd9f40761807db8e74e475b6116ec
NicholasA06 <nic@DESKTOP-21GIDM5.localdomain>
2025-08-08 22:48:47 +0930

    Prac 2 Reload

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -3,8 +3,8 @@
 void print_scaled(int array[3][3], int scale) {
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
-            cout << array[i][j] * scale;
-            if (j < 2) cout << " ";
+            std::cout << array[i][j] * scale;
+            if (j < 3 - 1) std::cout << " ";
         }
         std::cout << "\n";
     }
--- main-1-5.cpp
+++ main-1-5.cpp
@@ -1,14 +1,17 @@
+#include <iostream>
+
+void print_summed(int array1[3][3], int array2[3][3]);
 int main() {
     int matrix1[3][3] = {
-        {5,  -2,  7},
-        {0,   4, -1},
-        {8,   3,  6}
+        {5, -2, 7},
+        {0,  4, -1},
+        {8,  3,  6}
     };
     int matrix2[3][3] = {
-        {1,   9, -4},
-        {7,  -3,  2},
-        {-2,  0,  5}
+        {1,  9, -4},
+        {7, -3,  2},
+        {-2, 0,  5}
     };
-
     print_summed(matrix1, matrix2);
-    }
+    return 0;
+}
